69b3fe7f0603137f5190d7a4bdb3ee9cefa782f0
jose <jopineiro12@gmail.com>
2024-06-10 01:42:21 -0300

    Se añadio el modo oscuro y claro, modularizacion de instalaciones, cambio tamaño header, se añadio mapa

--- web/app.py
+++ web/app.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    imagenes=["Imagenes/presencialunarportada.jpg","Imagenes/vicariaameliaportada.jpg","Imagenes/bestiaclerigoportada.jpg","Imagenes/emisariocelestialportada.jpg","Imagenes/adelaportada.jpg","Imagenes/almendraportada.jpg"]
-    return render_template("base.html",lista=imagenes)
+    imagenes=["Imagenes/presencialunarportada.jpg","Imagenes/vicariaameliaportada.jpg","Imagenes/bestiaclerigoportada.jpg",
+              "Imagenes/emisariocelestialportada.jpg","Imagenes/adelaportada.jpg","Imagenes/almendraportada.jpg"]
+    espacios={"Pileta común":["Para dias calurosos podras disfrutar de nuestra piscina!!",'Imagenes/pileta.jpg']
+              ,"Sala de SPA":["Una increible sala de spa para que puedas relajarte despues de un dia muy largo de haber conocido la zona!!",'Imagenes/spa.jpg'],
+              "Arcade con cocina":["Podras disfrutar de todos los juegos que tenemos para ti! pool, videojuegos, television, metegol y muchos juegos de mesa!!",'Imagenes/juegos.jpg']}
+    ubicacion="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3424.447722353874!2d-64.52246542441412!3d-30.87413387451641!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a3be8784d545%3A0x3117451a980afefd!2sCaba%C3%B1as%20La%20Morada%20De%20La%20Luna!5e0!3m2!1ses!2sar!4v1717986682657!5m2!1ses!2sar"
+    return render_template("base.html",lista_carrucel=imagenes,dic_espacios=espacios,ubicacion=ubicacion)
 
 
 # Ruta para manejar la solicitud de reserva
@@ -36,6 +41,9 @@ def forms_reserva():
 def reserva():
     return render_template("reserva.html")
 
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
 
 
 
--- web/static/js/script.js
+++ web/static/js/script.js
@@ -45,14 +45,49 @@ function toggleText() {
 let hideText_btn2 = document.getElementById('hideText_btn2');
 let hideText2 = document.getElementById('hideText2');
 
-hideText_btn2.addEventListener('click', toggleText);
-function toggleText() {
+hideText_btn2.addEventListener('click', toggleText2);
+function toggleText2() {
     hideText2.classList.toggle('show');
     if(hideText2.classList.contains('show')) {
       hideText_btn2.innerHTML = 'Leer menos';
     } else {
       hideText_btn2.innerHTML = 'Mas información';
     }
+}
+
+let dark_mode = false;
+let dark_mode_btn = document.getElementById('moon_btn');
+let modeFa = document.getElementById('modeFa');
+let white_elements = document.getElementsByClassName("bc-white");
+let dark_elements = document.getElementsByClassName("bc-light-gray");
+
+dark_mode_btn.addEventListener('click',toggleDarkMode);
+function toggleDarkMode() {
+  dark_mode = !dark_mode;
+  if(dark_mode) {
+    modeFa.classList.remove('fa-sun-o');
+    modeFa.classList.add('fa-moon-o');
+    for (var i = 0; i < white_elements.length; i++) {
+      white_elements[i].style.backgroundColor="rgb(31, 31, 31)";
+      white_elements[i].style.color="white";
+    }
+    for (var i = 0; i < dark_elements.length; i++) {
+      dark_elements[i].style.backgroundColor="rgb(51, 51, 51)";
+    }
+    console.log(dark_mode);
+  } else {
+    console.log(dark_mode);
+    modeFa.classList.add('fa-sun-o');
+    modeFa.classList.remove('fa-moon-o');
+    for (var i = 0; i < white_elements.length; i++) {
+      white_elements[i].style.backgroundColor="white";
+      white_elements[i].style.color="black";
+    }
+    for (var i = 0; i < dark_elements.length; i++) {
+      dark_elements[i].style.backgroundColor="rgb(31, 31, 31)";
+    }
+
+  }
 }
 
 function togglecabaña(cabaña) {
