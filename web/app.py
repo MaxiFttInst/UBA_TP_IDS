cd3dface3d8039ec5cdc688a009e3751419a89ec
Mateo Parrado <mateoparradoo@gmail.com>
2024-06-12 17:00:53 -0300

    cambio nombres de variables

--- web/app.py
+++ web/app.py
@@ -34,7 +34,7 @@ def forms_reserva():
     if response.status_code == 201:
         return render_template("form_exitoso.html")
     else:
-        return render_template("form_exitoso.html")
+        return render_template("form_fallido.html")
 
 
 @app.route("/reserva")
@@ -47,9 +47,9 @@ def reserva():
 def exito():
     return render_template("form_exitoso.html")
 
-@app.route("/formno")
-def form_no():
-    return render_template("form_no.html")
+@app.route("/cancelacion_fallida")
+def cancelacion_fallida():
+    return render_template("form_fallido.html")
 
 
 if __name__ == "__main__":
