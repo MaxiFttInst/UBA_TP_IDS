541c25ba34da86ccb6a8f3233b390cff4d3c35d0
Mateo Parrado <mateoparradoo@gmail.com>
2024-06-16 11:39:24 -0300

    Implementación de Estilos Dinámicos Según Mensajes Flash

--- web/app.py
+++ web/app.py
@@ -3,6 +3,7 @@ import requests
 import os
 import utils
 app = Flask(__name__)
+app.secret_key = b'clave_secreta_byteados'  #necesaria para usar flash  
 
 API_URL = os.environ.get("API_URL", "https://posadabyteados.pythonanywhere.com")
 UBICACION = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3424.447722353874!2d-64.52246542441412!3d-30.87413387451641!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a3be8784d545%3A0x3117451a980afefd!2sCaba%C3%B1as%20La%20Morada%20De%20La%20Luna!5e0!3m2!1ses!2sar!4v1717986682657!5m2!1ses!2sar"
@@ -44,12 +45,14 @@ def forms_reserva():
 
     # Realizar la solicitud POST a la API del backend
     response = requests.post(API_URL + "/crear_reserva", json=data)
+    print("Respuesta de la API:", response.text)
 
     if response.status_code == 201:
-        print("Respuesta de la API:", response.text)
         codigo_reserva = response.json().get('codigo_reserva')
+        flash('EXITO')  # Configurar el mensaje flash
         return render_template("reserva_exitosa.html", codigo_reserva=codigo_reserva)
     else:
+        flash('ERROR')
         return render_template("reserva_fallida.html")
 
 
@@ -70,8 +73,10 @@ def forms_cancelacion():
     print("Respuesta de la API:", response.text)
 
     if response.status_code == 202:
+        flash('EXITO')
         return render_template("cancelacion_exitosa.html")
     else:
+        flash('ERROR')
         return render_template("cancelacion_fallida.html")
     
 @app.route('/consultar-reserva', methods=['POST'])
@@ -108,3 +113,9 @@ def cancelar():
 
 if __name__ == "__main__":
     app.run("127.0.0.1", port="8080", debug=True)
+
+
+
+@app.route("/reserva_exitosa")
+def reserva_exitosa():
+    return render_template("reserva_exitosa.html")
